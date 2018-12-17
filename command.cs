d6fc2b70f0ee5ce7dd57f8666c334fed9cb4c477
Rajahmed Sayyad <35951486+rajahmedsayyad@users.noreply.github.com>
2018-12-17 12:57:28 +0530

    Create command.cs

---
+++ command.cs
@@ -0,0 +1,12 @@
+namespace command
+{
+class program
+{
+public static void Main(string []ar)
+{
+System.Console.WriteLine("First name is "+ar[0]);
+System.Console.WriteLine("Last name is "+ar[1]);
+System.Console.ReadLine();
+}
+}
+}
